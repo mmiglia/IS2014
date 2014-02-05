828decc9bad4fb65ef2f99458e3f98ea4275a896
mmiglia <mauro.migliardi@gmail.com>
2014-02-05 11:26:34 +0100

    Test di autopruning sul tempo passato in attesa

---
+++ net/CharServerMT.java
@@ -0,0 +1,147 @@
+package net;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class CharServerMT
+{
+	List<Socket> l;
+	int port;
+	ServerSocket ss = null;
+	int idleHandler = 0;
+
+	public static void main(String[] argv)
+	{
+		(new CharServerMT(argv)).exec();
+	}
+
+	public CharServerMT(String[] argv)
+	{
+		port = 5678;
+		if(argv.length > 0)
+		{
+			port = Integer.parseInt(argv[0]);
+		}
+
+		try
+		{
+			ss = new ServerSocket(port);
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+			System.exit(1);
+		}
+
+		l = new LinkedList<Socket>();
+	}
+
+	void exec()
+	{
+		while(true)
+		{
+			try
+			{
+				System.out.println("Accetto connessioni...");
+				Socket s = ss.accept();
+				System.out.println("Ricevuta connessione");
+
+				synchronized(l)
+				{
+					l.add(s);
+					l.notify();
+					if(idleHandler < l.size())
+					{
+						(new Thread(new Handler(l, this))).start();
+					}
+				}
+			}
+			catch(IOException ioe)
+			{
+				ioe.printStackTrace();
+			}
+		}
+	}
+
+	class Handler
+	implements Runnable
+	{
+		List<Socket> q;
+		Socket s = null;
+		CharServerMT csmt = null;
+
+		Handler(List<Socket> theQ, CharServerMT theS)
+		{
+			System.out.println("Creo Handler");
+			q = theQ;
+			csmt = theS;
+		}
+
+		public void run()
+		{
+			long waitingTime = 10000;
+			while(true)
+			{
+				s = null;
+				BufferedReader br = null;
+				PrintStream ps = null;
+				synchronized(q)
+				{
+					while(q.isEmpty())
+					{
+						try
+						{
+							csmt.idleHandler++;
+							long before = System.currentTimeMillis();
+							q.wait(waitingTime);
+							long now = System.currentTimeMillis();
+							csmt.idleHandler--;
+							if( (now-before) >= waitingTime)
+							{
+								//sono andato in timeout
+								System.out.println("Handler dies");
+								return;
+							}
+						}
+						catch(InterruptedException ie)
+						{
+							ie.printStackTrace();
+						}
+					}
+					s = q.remove(0);
+				}
+
+				try
+				{
+					br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+					ps = new PrintStream(s.getOutputStream());
+	
+					String line = null;
+					while( (line = br.readLine()) != null)
+					{
+						System.out.println("Mi hanno detto <" + line + ">");
+						ps.println("Mi hai detto <" + line + ">");
+					}
+				}
+				catch(IOException ioe)
+				{
+					ioe.printStackTrace();
+				}
+				finally
+				{
+					try
+					{
+						br.close();
+						ps.close();
+						s.close();
+					}
+					catch(IOException ioe)
+					{
+						ioe.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
